edb32a4bf8251293ce450f7aaa661865a2a74e1d
Mateus F. Cunha <mateusfranca0220@gmail.com>
2023-08-28 20:36:11 -0300

    Criação do projeto Bikcraft

---
+++ js/script.js
@@ -0,0 +1,61 @@
+// Ativar itens do orçamento
+
+const params = new URLSearchParams(location.search);
+
+params.forEach((item) => {
+  const element = document.querySelector(`[type="radio"][value="${item}"]`);
+  if (element) element.checked = true;
+});
+
+const dts = document.querySelectorAll("dt");
+
+dts.forEach((dt) => {
+  dt.addEventListener("click", () => {
+    dt.parentElement.classList.toggle("ativa");
+  });
+});
+
+// Ativar links do menu: 
+const links = document.querySelectorAll(".header-menu a");
+
+links.forEach((link) => {
+  if (location.href.includes(link.href)) {
+    link.classList.add("ativo");
+  }
+});
+
+// Galeria de Bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+galeria.forEach((img) => {  
+  img.addEventListener("click", () => {
+    if (matchMedia("(min-width: 1000px)").matches) {
+      galeriaContainer.prepend(img);
+    }
+  });
+});
+
+// Perguntas Frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// Animação
+
+if (window.SimpleAnime) new SimpleAnime();
